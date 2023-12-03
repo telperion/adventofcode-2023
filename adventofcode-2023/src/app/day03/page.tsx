7ab00c1e3e35a6791116218082256036810dbe1d
Telperion <twice.invisible@gmail.com>
2023-12-02 22:08:09 -0800

    03.1 works!

---
+++ adventofcode-2023/src/app/day03/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+const title = "Day 3: Gear Ratios"
+
+import { listeners } from 'process';
+import React, { useEffect, useState } from 'react';
+import FileDrop from 'src/app/refs/filedrop';
+
+export default function Day01Component() {
+    const [data, setData] = useState<string>("");
+    const [result1, setResult1] = useState<string>("");
+    const [result2, setResult2] = useState<string>("");
+
+    useEffect(() => {
+        /*************************************************************/
+        // Part 1 begin
+
+        type PartType = {
+            x: number,
+            y: number,
+            part: string,
+            isPart: boolean
+        };
+
+        var potentialParts: Array<PartType> = [];
+        var parts: Array<PartType> = [];
+
+        var lines = data.split("\n")
+        lines.forEach( (l, y) => {
+            var newPart: PartType | null = null;
+            l.split("").forEach( (c, x) => {
+                if (c >= "0" && c <= "9")
+                {
+                    if (!newPart)
+                    {
+                        newPart = {
+                            x: x,
+                            y: y,
+                            part: "",
+                            isPart: false
+                        };
+                    }
+                    newPart.part += c;
+                }
+                else
+                {
+                    if (newPart)
+                    {
+                        potentialParts.push(newPart);
+                        newPart = null;
+                    }
+                }
+            })
+            if (newPart)
+            {
+                potentialParts.push(newPart);
+                newPart = null;
+            }
+        })
+        console.log(potentialParts.length)
+
+        lines.forEach( (l, y) => {
+            l.split("").forEach( (c, x) => {
+                if (c >= "0" && c <= "9") {}
+                else if (c != ".")
+                {
+                    // Adjacency requirement
+                    for (let i = potentialParts.length - 1; i >= 0; i--) {
+                        var part = potentialParts[i];
+                        var dx = x - part.x;
+                        var dy = y - part.y;
+                        if (dy >= -1 && dy <= 1)
+                        {
+                            // On the same or adjacent line
+                            if (dx >= -1 && dx <= part.part.length)
+                            {
+                                // Within one column of beginning-to-end range of part number
+                                part.isPart = true;
+                                potentialParts.splice(i, 1);
+                                parts.push(part);
+                                console.log(`Part (${part.x}~${part.x+part.part.length-1}, ${part.y}: ${part.part}) confirmed by symbol "${c}" at (${x}, ${y})`)
+                            }
+                        }
+                    }
+                }
+            })
+        })
+
+        var totalPartNumbers = 0;
+        parts.forEach( (part) => {
+            totalPartNumbers += parseInt(part.part);
+        })
+        potentialParts.forEach( (part) => {
+            console.log(`Not a part: (${part.x}~${part.x+part.part.length-1}, ${part.y}: ${part.part})`)
+        })
+        console.log(parts.length)
+        console.log(potentialParts.length)
+        
+
+        setResult1(totalPartNumbers.toString());
+
+        // Part 1 end
+        /*************************************************************/
+        // Part 2 begin
+
+
+        // Part 2 end
+        /*************************************************************/
+    }, [data])
+    
+    return (
+        <div className="h-screen flex flex-col items-center justify-center gap-6">
+            <div className="flex text-3xl">{title}</div>
+            <FileDrop passData={(d: string) => {setData(d)}} />
+            <div className="flex basis-1/12 flex-row items-center justify-center w-3/4">
+                <div className="min-w-fit p-6">Result 1:&nbsp;</div>
+                <div className="grow text-right bg-slate-900 p-6">{result1}</div>
+            </div>
+            <div className="flex basis-1/12 flex-row items-center justify-center w-3/4">
+                <div className="min-w-fit p-6">Result 2:&nbsp;</div>
+                <div className="grow text-right bg-slate-900 p-6">{result2}</div>
+            </div>
+        </div>
+    )
+}
