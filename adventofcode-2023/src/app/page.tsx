ba12778090d9fbc1e352ca4ad99bfa86fa75b772
Telperion <twice.invisible@gmail.com>
2023-12-02 22:49:38 -0800

    ??????

--- adventofcode-2023/src/app/page.tsx
+++ adventofcode-2023/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Navigation() {
     <main className="h-screen flex flex-col items-center justify-center gap-6">
       <div className="text-center">
         <p className="text-2xl">Advent of Code 2023</p>
-        <p className="text-xl">Telperion's solutions</p>
+        <p className="text-xl">Telperion&apos;s solutions</p>
       </div>
       {days.map((d) => (
           <Link key={d} href={`/day${d}`} className="rounded-md min-w-fit bg-slate-900 hover:bg-slate-600 p-6">Day {d}</Link>
