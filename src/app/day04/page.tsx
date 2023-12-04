c8a04c9819786163648f6c0ea139c5e1f603d4c1
Telperion <twice.invisible@gmail.com>
2023-12-03 21:49:38 -0800

    04.1 works!

---
+++ src/app/day04/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+const title = "Day 4: Scratchcards"
+
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link'
+import FileDrop from '../refs/filedrop';
+
+export default function Day01Component() {
+    const [data, setData] = useState<string>("");
+    const [result1, setResult1] = useState<string>("");
+    const [result2, setResult2] = useState<string>("");
+
+    useEffect(() => {
+        /*************************************************************/
+        // Part 1 begin
+
+        type ScratchType = {
+            index: number,
+            winners: Array<number>,
+            choices: Array<number>,
+            score: number
+        };
+
+        var scratches: Array<ScratchType> = [];
+
+        var lines = data.split("\n")
+        lines.forEach( (l) => {
+            var cardMatch = l.match(/Card\s+(\d+):(.*)\|(.*)/)
+            if (!cardMatch || cardMatch.length < 4)
+            {
+                console.log(`"${l}" doesn't match the scratcher format`)
+                return;
+            }
+
+            var index = parseInt(cardMatch[1])
+            var winners: Array<number> = []
+            cardMatch[2].split(/\s+/).forEach( (s) => {if (s != "") {winners.push(parseInt(s))}} )
+            var choices: Array<number> = []
+            cardMatch[3].split(/\s+/).forEach( (s) => {if (s != "") {choices.push(parseInt(s))}} )
+            var matches: Array<number> = []
+            winners.forEach( (w) => {if (choices.includes(w)) {matches.push(w)}} )
+            var newScratch: ScratchType = {
+                index: index,
+                winners: winners,
+                choices: choices,
+                score: (matches.length == 0) ? 0 : 2**(matches.length-1)
+            }
+            console.log(`${index}: ${matches} = ${newScratch.score}`)
+            scratches.push(newScratch)
+        })
+        
+        var totalScore = 0;
+        scratches.forEach( (scratch) => {
+            totalScore += scratch.score;
+        })
+
+        setResult1(totalScore.toString());
+
+        // Part 1 end
+        /*************************************************************/
+        // Part 2 begin
+
+        //setResult2(totalScore.toString());
+
+        // Part 2 end
+        /*************************************************************/
+    }, [data])
+    
+    return (
+        <div className="h-screen flex flex-col items-center justify-center gap-6">
+            <div className="flex text-3xl">{title}</div>
+            <FileDrop passData={(d: string) => {setData(d)}} />
+            <div className="flex basis-1/12 flex-row items-center justify-center w-3/4">
+                <div className="min-w-fit p-6">Result 1:&nbsp;</div>
+                <div className="grow text-right bg-slate-900 p-6">{result1}</div>
+            </div>
+            <div className="flex basis-1/12 flex-row items-center justify-center w-3/4">
+                <div className="min-w-fit p-6">Result 2:&nbsp;</div>
+                <div className="grow text-right bg-slate-900 p-6">{result2}</div>
+            </div>
+            <Link className="flex text-xl underline text-slate-400 hover:text-slate-100" href="/">Return home</Link>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,34 @@
 
 import Link from 'next/link'
 
-const latestDay = 3;
+const latestDay = 4;
+const blankLead = 5;
 var days: Array<string> = [];
 for (let i = 1; i <= latestDay; i++)
 {
   days.push(String(i).padStart(2, "0"));
 }
+var blanks: Array<int> = [];
+for (let i = 1; i <= blankLead; i++)
+{
+  blanks.push(i);
+}
  
 export default function Navigation() {
   return (
-    <main className="h-screen flex flex-col items-center justify-center gap-6">
-      <div className="text-center">
+    <main className="h-screen m-auto">
+      <div className="w-screen text-center p-6">
         <p className="text-2xl">Advent of Code 2023</p>
         <p className="text-xl">Telperion&apos;s solutions</p>
       </div>
-      {days.map((d) => (
-          <Link key={d} href={`/day${d}`} className="rounded-md min-w-fit bg-slate-900 hover:bg-slate-600 p-6">Day {d}</Link>
-      ))}
+      <div className="w-5/6 mx-auto grid grid-cols-7 grid-flow-row-dense gap-6">
+        {blanks.map((i) => (
+            <div key={`blank-${i}`}className="rounded-md min-w-fit bg-slate-900 p-6"></div>
+        ))}
+        {days.map((d) => (
+            <Link key={d} href={`/day${d}`} className="rounded-md min-w-fit text-center bg-slate-800 hover:bg-slate-500 p-6">Day {d}</Link>
+        ))}
+      </div>
     </main>
   )
 }
