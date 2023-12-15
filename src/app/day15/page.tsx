cca501467e9257743a63b7a18df957eab0de5c7d
Telperion <twice.invisible@gmail.com>
2023-12-15 14:48:26 -0800

    15.2 works!

--- src/app/day15/page.tsx
+++ src/app/day15/page.tsx
@@ -9,6 +9,115 @@ import FileDrop from '../refs/filedrop'
 const verbose = false
 const LOOP_SENTINEL = 1000000000
 
+class Lens {
+    label: string
+    focalLength: number
+
+    constructor(l: string, f: number) {
+        this.label = l
+        this.focalLength = f
+    }
+
+    toString() {
+        return `[${this.label} ${this.focalLength}]`
+    }
+}
+class ReindeerHashMap {
+    hashmap: Map<number, Array<Lens>>
+
+    constructor() {
+        this.hashmap = new Map<number, Array<Lens>>()
+    }
+
+    static hash(label: string) {
+        return label.split("").reduce((h: number, c: string) => {
+            return ((h + c.charCodeAt(0)) * 17) % 256
+        }, 0)
+    }
+
+    append(lens: Lens) {
+        // If the operation character is an equals sign (=), 
+        // it will be followed by a number indicating the focal length
+        // of the lens that needs to go into the relevant box;
+        // be sure to use the label maker to mark the lens with
+        // the label given in the beginning of the step so you can
+        // find it later. There are two possible situations:
+
+        var h = ReindeerHashMap.hash(lens.label)
+        var boxContents: Array<Lens> = this.hashmap.get(h) || []
+        var labelIndex = boxContents.map((l) => (l.label)).indexOf(lens.label)
+        if (labelIndex != -1) {
+            // If there is already a lens in the box with the same label,
+            // replace the old lens with the new lens:
+            // remove the old lens and put the new lens in its place,
+            // not moving any other lenses in the box.
+            console.log(`${lens} replaces ${boxContents[labelIndex]} in box ${h}`)
+            boxContents[labelIndex] = lens
+        }
+        else {
+            // If there is not already a lens in the box with the same label,
+            // add the lens to the box immediately behind any lenses already in the box.
+            // Don't move any of the other lenses when you do this.
+            // If there aren't any lenses in the box,
+            // the new lens goes all the way to the front of the box.
+            boxContents.push(lens)
+            console.log(`${lens} added into box ${h}`)
+        }
+        this.hashmap.set(h, boxContents)
+    }
+
+    remove(label: string) {
+        // If the operation character is a dash (-),
+        // go to the relevant box and remove the lens with
+        // the given label if it is present in the box.
+        // Then, move any remaining lenses as far forward in
+        // the box as they can go without changing their order,
+        // filling any space made by removing the indicated lens.
+        // (If no lens in that box has the given label, nothing happens.)
+        var h = ReindeerHashMap.hash(label)
+        var boxContents: Array<Lens> = this.hashmap.get(h) || []
+        var labelIndex = boxContents.map((l) => (l.label)).indexOf(label)
+        if (labelIndex != -1) {
+            boxContents.splice(labelIndex, 1)
+            console.log(`${label} removed from box ${h}`)
+        }
+        else {
+            console.log(`${label} already not in box ${h}`)
+        }
+        this.hashmap.set(h, boxContents)
+    }
+
+    op(info: string) {
+        var lensParts = info.match(/(\w+)([=-])(\d*)/)
+        if (lensParts && lensParts.length == 4) {
+            if (lensParts[2] == "=") {
+                this.append(new Lens(lensParts[1], parseInt(lensParts[3])))
+            }
+            else if (lensParts[2] == "-") {
+                this.remove(lensParts[1])
+            }
+        }
+    }
+
+    sum(): number {
+        var totalFocalCharacteristics = 0
+        for (let k of this.hashmap.entries()) {
+            for (let i = 0; i < k[1].length; i++) {
+                totalFocalCharacteristics += (k[0] + 1) * (i + 1) * k[1][i].focalLength
+            }
+        }
+        return totalFocalCharacteristics
+    }
+
+    toString(): string {
+        var lensStack = ""
+        for (let k of this.hashmap.entries()) {
+            lensStack += `Box ${k[0]}: ${k[1].map((l) => (l.toString())).join(" ")}\n`
+        }
+        return lensStack
+    }
+}
+
 
 export default function Day15Component() {
     const [data, setData] = useState<string>("")
@@ -26,27 +135,21 @@ export default function Day15Component() {
         // Part 1 begin
 
         var steps = lines.join("").split(",")
+
         var totalHash = 0
-        const hashAppend = (h: number, c: string) => {
-            h += c.charCodeAt(0)
-
-        }
-        steps.forEach((s) => {
-            var hash = s.split("").reduce((h: number, c: string) => {
-                return ((h + c.charCodeAt(0)) * 17) % 256
-            }, 0)
-            console.log(`${s}: ${hash}`)
-            totalHash += hash
-        })
-
+        steps.forEach((s) => {totalHash += ReindeerHashMap.hash(s)})
         setResult1(totalHash.toString())
         setDebugDisplay(debugAcc)
 
         // Part 1 end
         /*************************************************************/
-        // Part 2 begin       
+        // Part 2 begin
 
-        //setResult2(totalMirrorScore2.toString())
+        var lensLibrary = new ReindeerHashMap()
+        steps.forEach((s) => {lensLibrary.op(s)})
+        console.log(lensLibrary.toString())
+
+        setResult2(lensLibrary.sum().toString())
         setDebugDisplay(debugAcc)
 
         // Part 2 end
