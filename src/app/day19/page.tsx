dbebb962018a0d2de00113b64315862be3eed671
Telperion <twice.invisible@gmail.com>
2023-12-22 14:56:48 -0800

    19.2 works!
    I STOPPED MY LOOP TOO EARLY. ROOKIE MISTAKE

--- src/app/day19/page.tsx
+++ src/app/day19/page.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import FileDrop from '../refs/filedrop'
 
 const verbose = true
-const LOOP_SENTINEL = 1000
+const LOOP_SENTINEL = 100000
 
 class Rating {
     params: Map<string, number>
@@ -45,6 +45,15 @@ type FilterSpec = {
     dest: string
 }
 
+const fsString = (r: FilterSpec) => {
+    if (r.comp == "") {
+        return r.dest
+    }
+    else {
+        return `${r.comp}${r.isLT ? "<" : ">"}${r.limit}:${r.dest}`
+    }
+}
+
 class Workflow {
     rules: Array<FilterSpec>
 
@@ -77,14 +86,66 @@ class Workflow {
     }
 
     toString(): string {
-        return "{" + this.rules.map((r) => {
-            if (r.comp == "") {
-                return r.dest
-            }
-            else {
-                return `${r.comp}${r.isLT ? "<" : ">"}${r.limit}:${r.dest}`
-            }
-        }).join(",") + "}"
+        return "{" + this.rules.map((r) => (fsString(r))).join(",") + "}"
+    }
+}
+
+class Range {
+    min: Map<string, number>
+    max: Map<string, number>
+
+    constructor() {
+        this.min = new Map<string, number>()
+        this.max = new Map<string, number>()
+        for (let p of "xmas".split("")) {
+            this.min.set(p, 1)
+            this.max.set(p, 4001)           // Non-inclusive
+        }
+    }
+
+    copy(): Range {
+        var r = new Range()
+        for (let p of "xmas".split("")) {
+            r.min.set(p, this.min.get(p)!)
+            r.max.set(p, this.max.get(p)!)
+        }
+        return r
+    }
+
+    toString(): string {
+        return "{" + Array.from(this.min.keys()).map((k) => 
+            `${k}=${this.min.get(k)!.toString().padStart(4)}~${(this.max.get(k)!-1).toString().padStart(4)}`
+        ).join(", ") + "}"
+    }
+
+    split(p: string, v: number): Array<Range> {
+        var lh = this.copy()
+        var rh = this.copy()
+        if ("xmas".includes(p)) {
+            lh.max.set(p, Math.min(lh.max.get(p)!, Math.max(lh.min.get(p)!, v)))
+            rh.min.set(p, Math.max(rh.min.get(p)!, Math.min(rh.max.get(p)!, v)))
+        }
+        return [lh, rh]
+    }
+
+    combos(): number {
+        return Array.from(this.min.keys()).reduce(
+            (t, k) => (t * Math.max(this.max.get(k)! - this.min.get(k)!, 0)), 1
+        )
+    }
+}
+
+class Swath {
+    workflow: string
+    range: Range
+
+    constructor(wf: string, r: Range) {
+        this.workflow = wf
+        this.range = r
+    }
+
+    toString(): string {
+        return `${this.workflow}: ${this.range.toString()}`
     }
 }
 
@@ -131,6 +192,65 @@ class Sorter {
 
     accepted(part: Rating): boolean {
         return (this.flow(part) == "A")
+    }
+
+    walk(): number {
+        var totalAccepted = 0
+        var totalRejected = 0
+
+        var swaths: Array<Swath> = [new Swath("in", new Range())]
+        for (let i = 0; i < LOOP_SENTINEL && swaths.length > 0; i++) {
+            var currentSwath = swaths.shift()!
+            if (currentSwath.workflow == "A") {
+                var combos = currentSwath.range.combos()
+                totalAccepted += combos
+                console.log(`${currentSwath.toString()} accepted (+${combos})`)
+                continue
+            }
+            else if (currentSwath.workflow == "R") {
+                var combos = currentSwath.range.combos()
+                totalRejected += combos
+                console.log(`${currentSwath.toString()} rejected (-${combos})`)
+                continue
+            }
+            
+            var wf = this.workflows.get(currentSwath.workflow)
+            if (!wf) {
+                console.log("Undefined workflow?")
+                break
+            }
+
+            for (let r of wf.rules) {
+                if (r.comp == "") {
+                    console.log(`${currentSwath.workflow} ${fsString(r)} falls thru\n<-> ${currentSwath.range.toString()} :::(${currentSwath.range.combos().toString().padStart(16)})`)
+                    currentSwath.workflow = r.dest
+                    swaths.push(currentSwath)
+                }
+                else {
+                    var splitRanges = currentSwath.range.split(r.comp, r.limit + (r.isLT ? 0 : 1))  // Non-inclusive max
+                    console.log(`${currentSwath.workflow} ${fsString(r)} splits\n<-- ${splitRanges[0].toString()} :::(${splitRanges[0].combos().toString().padStart(16)})\n--> ${splitRanges[1].toString()} :::(${splitRanges[1].combos().toString().padStart(16)})`)
+                    if (r.isLT) {
+                        swaths.push(new Swath(r.dest, splitRanges[0]))
+                        currentSwath.range = splitRanges[1]
+                    }
+                    else {
+                        swaths.push(new Swath(r.dest, splitRanges[1]))
+                        currentSwath.range = splitRanges[0]
+                    }
+                }
+            }
+
+            /*
+            console.log(`${i}:`)
+            swaths.forEach((s) => {
+                console.log("--- " + s.toString())
+            })
+            */
+        }
+
+        console.log(`${totalAccepted} accepted + ${totalRejected} rejected = ${totalAccepted + totalRejected}`)
+
+        return totalAccepted
     }
 
     toString(): string {
@@ -182,8 +302,8 @@ export default function Day18Component() {
         /*************************************************************/
         // Part 2 begin
 
-        var lagoonArea2 = 0
-        setResult2(lagoonArea2.toString())
+        var totalCombinations = sorter.walk()
+        setResult2(totalCombinations.toString())
         setDebugDisplay(debugAcc)
 
         // Part 2 end
